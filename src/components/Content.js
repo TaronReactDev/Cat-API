89286e972a2e500688e831b119a576ee71b021a7
Taron <taronsargsyan.reactdev@gmail.com>
2023-01-04 04:35:01 +0400

    cat api scrol to top

--- src/components/Content.js
+++ src/components/Content.js
@@ -2,6 +2,7 @@ import React, {useEffect, useState} from "react";
 import {useDispatch, useSelector} from "react-redux";
 import axios from "axios";
 import Slider from "./Slider";
+import Arrow from "./icons/arrow.svg";
 
 export default function Content() {
     const dispatch = useDispatch();
@@ -10,6 +11,7 @@ export default function Content() {
     const category_id = useSelector(state => state.category_id);
 
     const [id, setId] = useState("");
+    const [showTopBtn, setShowTopBtn] = useState(false);
 
     const handleOneCat =(id)=>()=>{
         setId( id );
@@ -35,6 +37,13 @@ export default function Content() {
         })
     }
 
+    const handleScrollToTop=()=>{
+        window.scrollTo({
+            top: 0,
+            behavior: 'smooth',
+        });
+    }
+
     useEffect(() => {
         axios.get(`https://api.thecatapi.com/v1/images/search?limit=${limit}&page=1&category_ids=${category_id}`)
             .then(res => res)
@@ -44,6 +53,14 @@ export default function Content() {
                     cats: data.data,
                 }
             }));
+
+        window.addEventListener('scroll', () => {
+            if (window.scrollY > 400) {
+                setShowTopBtn(true);
+            } else {
+                setShowTopBtn(false);
+            }
+        });
 
     }, [])
 
@@ -59,9 +76,6 @@ export default function Content() {
 
     }, [limit,category_id])
 
-    console.log(limit,"limit")
-    console.log(catsState,"catsState")
-    console.log(category_id,"category_id")
 
     const cats = catsState.map((el) => <li key={el.id} className="content-container__ul__item" onClick={handleOneCat(el.id)}>
         <div style={{backgroundImage: `url(${el.url})`}} className="content-container__ul__item__img"/>
@@ -75,7 +89,10 @@ export default function Content() {
                 {cats}
             </ul>
 
-            { catsState.length   ? <button className="content-container__btn" onClick={handleMoreBtn}>SEE MORE</button>
+            { catsState.length   ? <> <button className="content-container__btn" onClick={handleMoreBtn}>SEE MORE</button>
+                    {showTopBtn ? <div className="scroll-to-top" onClick={handleScrollToTop}>
+                       <img alt="arrow" src={Arrow}/>
+                    </div> : ""} </>
                 :
                category_id==="none" ?   <div className="content-container__text" id="content-container__text" onClick={handleLoadMix}>
                     Choose your favorite category or click here to see mixed
